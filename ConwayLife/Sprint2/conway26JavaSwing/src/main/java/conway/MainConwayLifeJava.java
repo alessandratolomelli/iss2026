37e06248035545b52a49e5738aa8f97e0528a390
alessandratolomelli <alessandratolomelli@gmail.com>
2026-03-06 13:00:01 +0100

    aggiunta cartella ConwayLife

---
+++ ConwayLife/Sprint2/conway26JavaSwing/src/main/java/conway/MainConwayLifeJava.java
@@ -0,0 +1,27 @@
+package main.java.conway;
+import main.java.conway.devices.ConwayLifeGridClaude;
+import main.java.conway.domain.*;
+public class MainConwayLifeJava  {
+
+	public void configureTheSystemWitMockOutdev() {
+    }
+
+	public void configureTheSystemWithSwing() {
+        Life life            = new Life( 20,20 );             //ncell in iomap.js        
+        IOutDev swinggui     = new ConwayLifeGridClaude( );   //dispositivo di output e anche di input 
+        GameController  cc   = new LifeController(life, swinggui) ;   //un GameController che deve usare un outdev
+        ((ConwayLifeGridClaude) swinggui).setController(cc);          //iniezione del controller nella GUI
+        //Il sistema termina quando si chiude la swinggui
+   	}
+	
+	public void configureTheSystemWithHtmlWs(boolean pageexternal) {
+ 	}
+ 	
+    public static void main(String[] args) {
+    System.out.println("MainConway | STARTS " );  
+    MainConwayLifeJava app = new MainConwayLifeJava();
+    app.configureTheSystemWithSwing();
+    System.out.println("MainConway | ENDS " );  
+    }
+
+}
